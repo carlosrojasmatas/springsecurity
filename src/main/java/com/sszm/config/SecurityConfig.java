ec5fd6ccfe821578dcbe437b49b77aab20c85192
Carlos Rojas <carlos.rojasmatas@gmail.com>
2024-03-07 20:44:33 +0100

    UserDetails with embedded password encoder

--- src/main/java/com/sszm/config/SecurityConfig.java
+++ src/main/java/com/sszm/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.sszm.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,4 +25,22 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        var admin = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .authorities("admin")
+                .build();
+
+        var roUser = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("123")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, roUser);
+    }
+
 }
