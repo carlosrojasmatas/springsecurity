eceb32e853999dae78b452c9c641bfb81b06ceaa
Carlos Rojas <carlos.rojasmatas@gmail.com>
2024-04-09 17:04:50 +0200

    SQL based auth added

--- src/main/java/com/sszm/controller/RegistrationController.java
+++ src/main/java/com/sszm/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.sszm.model.Customer;
 import com.sszm.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,20 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/v1/registration")
+@RequestMapping("v1/registration")
 public class RegistrationController {
     private CustomerRepository customerRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationController(CustomerRepository customerRepository) {
+    public RegistrationController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping
-    public ResponseEntity<String> register(@RequestBody Customer requestCustomer) {
+    public ResponseEntity<String> register(@RequestBody Customer customer) {
         try {
-            var customer = customerRepository.save(requestCustomer);
-            return ResponseEntity.created(URI.create("/v1/registration/" + customer.getId())).body("Customer registered successfully");
+            var password = customer.getPwd();
+            var encodedPass = passwordEncoder.encode(password);
+            customer.setPwd(encodedPass);
+            return ResponseEntity.created(URI.create("/v1/registration/" + customerRepository.save(customer).getId())).body("Customer registered successfully");
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body("Customer registration failed");
         }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table users(
                       username varchar(50) not null primary key,
-                      password varchar(50) not null,
+                      password text not null,
                       enabled boolean not null
 );
 
@@ -14,8 +14,8 @@ create unique index ix_auth_username on authorities (username,authority);
 create table customer(
     id uuid primary key ,
     email varchar(50) not null,
-    pwd varchar(50) not null,
+    pwd text not null,
     role varchar(50) not null
 )
 
-insert into customer values ('03f0f5e4-283e-4b58-a6a8-32a0f5a6c6e5','test@test.com','12345','admin');
+-- insert into customer values ('03f0f5e4-283e-4b58-a6a8-32a0f5a6c6e5','test@test.com','12345','admin');
